6ddabd2e32951d3f8fd4afe885430c0ff2547713
Rémi Durieu <mail@remidurieu.dev>
2022-01-30 17:29:18 +0100

    Fix types

--- controllers/base_controller.ts
+++ controllers/base_controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
 class BaseController {
-  headers (_req: Request, res: Response, next) {
+  headers (_req: Request, res: Response, next: any) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Methods', '*')
