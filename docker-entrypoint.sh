e1ab6544fe854bcda1630ef9d53447391ee0e668
Rémi Durieu <mail@remidurieu.dev>
2022-03-12 11:08:41 +0100

    Fix Docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,13 @@
 set -e
 
 cd /app
-
+s
 if [ $MIGRATE_DATABASE -eq 1 ]; then
   echo "Running migrations..."
   npx prisma migrate deploy
+
+  echo "Seeding database..."
+  npx prisma db seed
 fi
 
 exec "$@"
