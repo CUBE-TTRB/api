c56afc33cfdf9b397558e2c876a05ae3b98f563d
Rémi Durieu <mail@remidurieu.dev>
2022-03-28 09:24:46 +0200

    Fix docker deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,14 +5,14 @@ set -e
 cd /app
 
 if [ $MIGRATE_DATABASE -eq 1 ]; then
+  echo "Generating Prisma Client..."
+  npx prisma generate
+
   echo "Running migrations..."
   npx prisma migrate deploy
 
   echo "Seeding database..."
   npx prisma db seed
-
-  echo "Generating Prisma Client..."
-  npx prisma generate
 fi
 
 exec "$@"
