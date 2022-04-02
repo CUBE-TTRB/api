9bbc6a6371bcaf1179dfb59a990168977c697c06
Rémi Durieu <mail@remidurieu.dev>
2022-04-02 17:37:33 +0200

    Fix hard coded values that sould be in ENV

--- config/config.ts
+++ config/config.ts
@@ -2,20 +2,23 @@ import 'dotenv/config'
 
 class Config {
   jwtSecret : string
-  confirm : boolean
+  hostUrl : string
+  sendgridUser : string
+  sendgridPassword : string
+  confirmNewUsers : string | undefined
   constructor () {
     this.jwtSecret = process.env.JWT_SECRET || ''
-    if ('CONFIRM' in process.env && process.env.CONFIRM !== undefined && process.env.CONFIRM === '1') {
-      this.confirm = true
-    } else {
-      this.confirm = false
-    };
+    this.hostUrl = process.env.HOST_URL || ''
+    this.sendgridUser = process.env.SENDGRID_USER || ''
+    this.sendgridPassword = process.env.SENDGRID_PASSWORD || ''
+    this.confirmNewUsers = process.env.CONFIRM_NEW_USERS
   }
 
   sanitize () : this {
-    if (this.jwtSecret === '') {
-      throw Error('Missing JWT_SECRET in env')
-    }
+    if (this.jwtSecret === '') throw Error('Missing JWT_SECRET in env')
+    if (this.hostUrl === '') throw Error('Missing HOST_URL in env')
+    if (this.sendgridUser === '') throw Error('Missing SENDGRID_USER in env')
+    if (this.sendgridPassword === '') throw Error('Missing SENDGRID_PASSWORD in env')
     return this
   }
 }
--- services/mail/send_mail_auth.ts
+++ services/mail/send_mail_auth.ts
@@ -2,6 +2,7 @@ import nodemailer from 'nodemailer'
 import SMTPTransport from 'nodemailer/lib/smtp-transport'
 import { ApplicationService } from '../application_service'
 import JwtHandler from '../../lib/Encryption/JwtHandler'
+import config from '../../config/config'
 
 // mdp sendgrid : cubecube76.99A*A
 // async..await is not allowed in global scope, must use a wrapper
@@ -17,7 +18,7 @@ export default class SendMailService extends ApplicationService {
   }
 
   async call () : Promise<this> {
-    let lien = 'http://localhost:3000/users/confirm/'
+    let link = `${config.hostUrl}/users/confirm/`
     // create reusable transporter object using the default SMTP transport
     let transporter : nodemailer.Transporter<SMTPTransport.SentMessageInfo>
     try {
@@ -26,8 +27,8 @@ export default class SendMailService extends ApplicationService {
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
-          user: 'apikey',
-          pass: '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'
+          user: config.sendgridUser,
+          pass: config.sendgridPassword
         }
       })
     } catch (e : any) {
@@ -39,16 +40,13 @@ export default class SendMailService extends ApplicationService {
 
     try {
       this._jwtToken = await JwtHandler.getToken(this._userId, 'none', 'validMail')
-      lien += this._jwtToken
-      lien += '?' + this._userId
+      link += this._jwtToken
+      link += '?' + this._userId
     } catch (e : any) {
       this.errors.push({
         error: e,
         message: 'erreur lors de la creation du token de validation'
       })
-    }
-
-    if (this.errors.length > 0) {
       return this
     }
 
@@ -59,7 +57,7 @@ export default class SendMailService extends ApplicationService {
         to: this._mail + ', ' + this._mail, // list of receivers
         subject: 'Confirmation de création de compte ✔', // Subject line
         text: 'Bonjour :D', // plain text body
-        html: '<b>CLiquez sur le lien de confirmation : <a href="' + lien + '">lien<a/></b>' // html body
+        html: '<b>CLiquez sur le lien de confirmation : <a href="' + link + '">lien<a/></b>' // html body
       })
     } catch (e : any) {
       this.errors.push({
