a5a1f0252a16cf10e0e744c584b8afb53196affd
Rémi Durieu <mail@remidurieu.dev>
2022-05-09 11:17:07 +0200

    Remove legacy error handling in ResourcsController

--- controllers/resources_controller.ts
+++ controllers/resources_controller.ts
@@ -1,25 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
 import { prisma } from '../app'
 import Paginator from '../lib/paginator'
-import { PrismaErrorAdapter } from '../lib/prisma_error_adapter'
 import Resource from '../models/resource'
-import { InvalidRecordError } from '../models/validators/predicates'
 
 class ResourcesController {
   async index (req: Request, res: Response, next: NextFunction) {
-    let records
-    try {
-      records = await prisma.resource.findMany()
-    } catch (error: any) {
-      res.status(500)
-      res.locals.errors.push(new PrismaErrorAdapter(error))
-      next(); return
-    }
-
+    const records = await prisma.resource.findMany()
     const paginator = new Paginator(req, records)
     res.locals.result = paginator.result
     res.locals.pagination = paginator.pagination
-    console.log(paginator)
     res.status(200)
     next()
   }
@@ -27,20 +16,7 @@ class ResourcesController {
   async create (req: Request, res: Response, next: NextFunction) {
     const params = Resource.permitParams(req.body.resource)
     const resource = new Resource(params)
-
-    try {
-      await resource.save()
-    } catch (error: any) {
-      if (error instanceof InvalidRecordError) {
-        res.status(422)
-        res.locals.errors.push(...resource.errors)
-        next(); return
-      } else {
-        res.status(500)
-        res.locals.errors.push(new PrismaErrorAdapter(error))
-        next(); return
-      }
-    }
+    await resource.save()
 
     res.status(201)
     res.locals.result = resource.record
@@ -48,45 +24,19 @@ class ResourcesController {
   }
 
   async show (req: Request, res: Response, next: NextFunction) {
-    let record
+    const record = await prisma.resource.findUnique({
+      where: { id: parseInt(req.params.id) }
+    })
 
-    try {
-      record = await prisma.resource.findUnique({
-        where: { id: parseInt(req.params.id) }
-      })
-    } catch (error: any) {
-      res.status(500)
-      res.locals.errors.push(new PrismaErrorAdapter(error))
-      next(); return
-    }
-
-    if (record === null) {
-      res.status(404)
-      next(); return
-    }
-
-    res.status(200)
     res.locals.result = record
+    res.status(200)
     next()
   }
 
   async update (req: Request, res: Response, next: NextFunction) {
-    let record
-
-    try {
-      record = await prisma.resource.findUnique({
-        where: { id: parseInt(req.params.id) }
-      })
-    } catch (error: any) {
-      res.status(500)
-      res.locals.errors.push(new PrismaErrorAdapter(error))
-      next(); return
-    }
-
-    if (record === null) {
-      res.status(404)
-      next(); return
-    }
+    const record = await prisma.resource.findUnique({
+      where: { id: parseInt(req.params.id) }
+    })
 
     const resource = new Resource(record)
     const params = Resource.permitParams({
@@ -94,19 +44,7 @@ class ResourcesController {
       ...req.body.resource
     })
 
-    try {
-      await resource.update(params)
-    } catch (error: any) {
-      if (error instanceof InvalidRecordError) {
-        res.status(422)
-        res.locals.errors.push(...resource.errors)
-        next(); return
-      } else {
-        res.status(500)
-        res.locals.errors.push(new PrismaErrorAdapter(error))
-        next(); return
-      }
-    }
+    await resource.update(params)
 
     res.status(200)
     res.locals.result = resource.record
@@ -114,35 +52,14 @@ class ResourcesController {
   }
 
   async destroy (req: Request, res: Response, next: NextFunction) {
-    let record
-
-    try {
-      record = await prisma.resource.findUnique({
-        where: { id: parseInt(req.params.id) }
-      })
-    } catch (error: any) {
-      res.status(500)
-      res.locals.errors.push(new PrismaErrorAdapter(error))
-      next(); return
-    }
-
-    if (record === null) {
-      res.status(404)
-      next(); return
-    }
+    const record = await prisma.resource.findUnique({
+      where: { id: parseInt(req.params.id) }
+    })
 
     const resource = new Resource(record)
+    await resource.destroy()
 
-    try {
-      await resource.destroy()
-    } catch (error: any) {
-      res.status(500)
-      res.locals.errors.push(new PrismaErrorAdapter(error))
-      next(); return
-    }
-
-    res.status(200)
-    res.locals.result = null
+    res.status(204)
     next()
   }
 }
