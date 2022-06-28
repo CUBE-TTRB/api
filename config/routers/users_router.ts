ad369bba9e8e0d43730c8af72c4cdbade243e5fe
Rémi Durieu <mail@remidurieu.dev>
2022-06-28 13:22:29 +0200

    Add new fields and actions for users (#40)
    * Add some fields ont User and show/update
    
    * Fix

--- config/routers/users_router.ts
+++ config/routers/users_router.ts
@@ -1,8 +1,14 @@
 import usersController from '../../controllers/users_controller'
 import express from 'express'
+import baseController from '@App/controllers/base_controller'
 
 const usersRouter = express.Router()
 export default usersRouter
 
 usersRouter.post('/', usersController.create)
+usersRouter.get('/:id', usersController.show)
+
+usersRouter.patch('/:id', baseController.tokenCheck)
+usersRouter.patch('/:id', usersController.update)
+
 usersRouter.get('/confirm/:token', usersController.confirm)
