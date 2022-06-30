8e1d937d4ed891a28efbe5288a3dbe3cfc07cb46
Rémi Durieu <mail@remidurieu.dev>
2022-06-30 11:18:16 +0200

    Remove console.log() and update Insomnia collection

--- controllers/resources_controller.ts
+++ controllers/resources_controller.ts
@@ -95,13 +95,11 @@ class ResourcesController {
         method: 'GET'
       })
       const textResponse = await (await response.text())
-      console.log(textResponse)
       keysToLinks[attachedfiles[i].key] = textResponse.slice(1, textResponse.length - 1)
     }
     if (record.body != null && typeof (record.body) === typeof (JSON) && attachedfiles.length > 0) {
       const recordBody = record.body as Prisma.JsonObject
       const bodyWithUrl = QuillHelper.replaceKeysByLinks(recordBody, keysToLinks)
-      console.log(bodyWithUrl)
       record.body = bodyWithUrl
     }
 
